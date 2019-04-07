3ced3183933ec313d27c79cd656aa4fc78941356
Xwilarg <xwilarg@yahoo.fr>
2019-04-08 00:01:27 +0200

    Add Library intro for Kancolle

--- js/display.js
+++ js/display.js
@@ -2,8 +2,10 @@ let http = new XMLHttpRequest();
 http.onreadystatechange = function() {
     if (this.readyState === 4 && this.status === 200) {
         let json = JSON.parse(this.responseText);
-        document.getElementById("imageKancolle").innerHTML = '<img src="' + json[0] + '" width="300">';
-        document.getElementById("imageAzurLane").innerHTML = '<img src="' + json[1] + '" width="300">';
+        document.getElementById("imageKancolle").innerHTML = '<img src="' + json[0] + '" width="300"/>';
+        document.getElementById("imageAzurLane").innerHTML = '<img src="' + json[1] + '" width="300"/>';
+        document.getElementById("audioIntroKancolle").innerHTML = '<audio controls src="' + json[2] + '"></audio>';
+        document.getElementById("textIntroKancolle").innerHTML = json[3];
     }
 }
 const urlParams = new URLSearchParams(window.location.search);
--- php/shipInfo.php
+++ php/shipInfo.php
@@ -6,14 +6,23 @@
         ]
     ];
     $context = stream_context_create($options);
-    $url = json_decode(file_get_contents("https://kancolle.wikia.com/api/v1/Search/List?query=" . urlencode($_GET['name']) . "&limit=1", false, $context))->items[0]->url . "/Gallery";
+
+    // base URL is the character page in the Wikia, url is to the gallery
+    $baseUrl = json_decode(file_get_contents("https://kancolle.wikia.com/api/v1/Search/List?query=" . urlencode($_GET['name']) . "&limit=1", false, $context))->items[0]->url;
+    $url = $baseUrl . "/Gallery";
     $kancolle = file_get_contents($url, false, $context);
     preg_match_all('/img src="([^"]+)"/', $kancolle, $matches);
-    $kancolleImage = $matches[1][1];
+    $kancolleImage = $matches[1][1]; // Character image
+    $kancolleMain = file_get_contents($baseUrl, false, $context);
+    preg_match('/<span[^>]+><a href="([^"]+)".+<\/span> Library\n.+<\/span>\n.+shipquote-en\">(.+)/', $kancolleMain, $matches);
+    $kancolleAudio = $matches[1]; // Character intro voiceline
+    $kancolleText = $matches[2]; // Character intro text
+
+    // url is the character page in the wikia
     $azurName = json_decode(file_get_contents("https://azurlane.koumakan.jp/w/api.php?action=opensearch&search=" . urlencode($_GET['name']) . "&limit=1", false, $context))[1][0];
     $url = "https://azurlane.koumakan.jp/" . $azurName;
     $azurLane = file_get_contents($url, false, $context);
     preg_match('/src="(\/w\/images\/thumb\/[^\/]+\/[^\/]+\/[^\/]+\/[0-9]+px-' . $azurName . '.png)/', $azurLane, $matches);
-    $azurLaneImage = "https://azurlane.koumakan.jp" . $matches[1];
-    echo(json_encode(array($kancolleImage, $azurLaneImage)));
+    $azurLaneImage = "https://azurlane.koumakan.jp" . $matches[1]; // Character image
+    echo(json_encode(array($kancolleImage, $azurLaneImage, $kancolleAudio, $kancolleText)));
 ?>
