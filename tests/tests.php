a3d6a80a3db832ac65c2d9f3775ba4b9bff47e1b
Xwilarg <xwilarg@yahoo.fr>
2019-04-08 23:28:43 +0200

    Fix tests

--- tests/tests.php
+++ tests/tests.php
@@ -1,5 +1,10 @@
 <?php
     require dirname(__FILE__) . '/../php/ShipInfo.php';
+
+    class tests extends PHPUnit\Framework\TestCase
+    { }
+    // We do that so PHP Unit load the empty class but still do the other tests
+
     function CheckShipExist($name) {
         $kancolle = ShipInfo::GetKancolleInfo($name);
         $azurLane = ShipInfo::GetAzurLaneInfo($name);
@@ -10,7 +15,7 @@
             PHPUnit\Framework\Assert::assertNotNull($elem, "Azur Lane, missing data for " . $name);
         }
     }
-    $arr = array("Fumizuki", "Akagi", "Ryuujou", "Kisaragi", "Akatsuki", "I-19", "Bismarck", "Ark Royal", "Akashi", "Zuikaku"); // We try with a lot of random ships 
+    $arr = array("Fumizuki", "Akagi", "Ryuujou", "Kisaragi", "Akatsuki", "I-19", "Warspite", "Ark Royal", "Akashi", "Zuikaku"); // We try with a lot of random ships
     foreach ($arr as $elem) {
         CheckShipExist($elem);
     }
