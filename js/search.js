369ea88ea1389a2b552c08ce52c07e027e2c4e08
Xwilarg <xwilarg@yahoo.fr>
2019-04-09 14:03:06 +0200

    Add basic autocomplete

--- js/search.js
+++ js/search.js
@@ -1,9 +1,30 @@
+var json = null;
+
 let http = new XMLHttpRequest();
 http.onreadystatechange = function() {
     if (this.readyState === 4 && this.status === 200) {
-        let json = JSON.parse(this.responseText);
+        json = JSON.parse(this.responseText);
     }
 }
 const urlParams = new URLSearchParams(window.location.search);
 http.open("GET", "php/getAllShips.php", true);
 http.send();
+
+document.getElementById("input").addEventListener("input", function(e) {
+    if (json !== null) {
+        let value = document.getElementById("input").value.toLowerCase();
+        if (value == "")
+            document.getElementById("autocomplete-all").innerHTML = '';
+        else {
+            let res = "";
+            let max = 5;
+            json.forEach(elem => {
+                if (max > 0 && elem.toLowerCase().startsWith(value)) {
+                    res += '<div id="autocomplete-elem">' + elem + '</div>';
+                    max--;
+                }
+            });
+            document.getElementById("autocomplete-all").innerHTML = res;
+        }
+    }
+});
